3fdc72ce4f7b85e84e4396c919405b4828b2d097
Thoriq Pratamaa <helloikha@gmail.com>
2023-12-13 14:02:13 +0700

    Initial commit

---
+++ src/features/user/LandingIntro.js
@@ -0,0 +1,16 @@
+function LandingIntro(){
+
+    return(
+        <div className="hero min-h-full rounded-l-xl bg-base-200">
+            <div className="hero-content py-12">
+              <div className="max-w-md">
+              <h1 className='text-3xl text-center font-bold '>Freyanation Admin Panel</h1>
+                <div className="text-center mt-12"><img src="./FN Logo (Double Stroke).png" alt="Dashwind Admin Template" className="w-48 inline-block"></img></div>
+              </div>
+            </div>
+          </div>
+    )
+      
+  }
+  
+  export default LandingIntro
